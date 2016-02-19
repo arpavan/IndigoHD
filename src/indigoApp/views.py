8ece8cf86e0def4ea9c8131d7a6bbe4c38e210e4
Pavan Agnihotri <you@example.com>
2016-02-19 18:46:13 -0500

    Unlinked virtualenv and made some changes to src

--- src/indigoApp/views.py
+++ src/indigoApp/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+	context = {'sample_text': "Hello World"}
+	return render(request, 'indigoApp/index.html', context)
+#    return HttpResponse("Hello, world!")
+
+def demo(request):
+	context = {'response_text': request.
+	return render(request, 'indigoApp/demo.html', context)
