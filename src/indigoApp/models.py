a2e9f6813cb58e4744bd36c0a4ca2e02eefeca5a
Pavan Agnihotri <you@example.com>
2016-02-19 17:12:33 -0500

    Created model for pro customer data

--- src/indigoApp/models.py
+++ src/indigoApp/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ProCustomers(models.Model):
+    CustomerID = models.IntegerField()
+    CompanyName = models.CharField(max_length=100)
+    CustomerSpend = models.DecimalField(max_digits=10, decimal_places=2)
+    Trade = models.CharField(max_length=100)
+    Zip = models.CharField(max_length=6)
+
+	@property
+    def CustomerSpend(self):
+        return "$%s" % self.CustomerSpend
+
