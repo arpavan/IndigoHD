c1b1cbf4261719847c18eb525736c52a84736842
gkamath <goutham.kamath@gmail.com>
2016-02-20 03:49:05 -0500

    Add pro user file

---
+++ src/indigoApp/getProUsers.py
@@ -0,0 +1,29 @@
+from keywordScrape import KeywordProvider
+from models import CategoryCustomerMap
+from models import Customers
+
+class ProUsers:
+
+    def getUsers(self, category, keyword):
+        if category:
+            return self.getUsersFromCategory(category)
+
+        if keyword:
+            keywords = KeywordProvider.getKeywords(KeywordProvider, keyword)
+            return self.getUsersFromKeyword(keywords)
+
+
+    def getUsersFromCategory(categoryInput):
+        for categoryCustomer in CategoryCustomerMap.objects.filter(category=categoryInput):
+            customers = Customers.objects.filter(Id=categoryCustomer.CustomerId)
+            for customer in customers:
+                print customer.Name
+        return
+
+    def getUsersFromKeyword(keyword):
+        return
+
+
+
+ProUsers.getUsers(ProUsers, "Appliances", None)
+
